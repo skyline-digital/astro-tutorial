78ae852e6eac795f659d9fad71712ecbbe41146d
jackdar <jackdarlington2001@gmail.com>
2025-09-29 10:30:21 +1300

    Fix issues with CDK

--- .infrastructure/cdk/bin/cdk.ts
+++ .infrastructure/cdk/bin/cdk.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { CdkStack } from "../lib/cdk-stack";
+import { AstroTutorialStack } from "../lib/cdk-stack";
 
 const app = new cdk.App();
-new CdkStack(app, "CdkStack", {
+new AstroTutorialStack(app, "AstroTutorialStack", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
--- .infrastructure/cdk/lib/cdk-stack.ts
+++ .infrastructure/cdk/lib/cdk-stack.ts
@@ -1,45 +1,47 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 
-export class CdkStack extends cdk.Stack {
+export class AstroTutorialStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const domainName = "astro-tutorial.aws.skylinedigital.co.nz"
-    const siteDomain = `www.${domainName}`
 
-    const hostedZone = new cdk.aws_route53.PublicHostedZone(this, "AstroTutorialHostedZone", {
+    const zone = new cdk.aws_route53.PublicHostedZone(this, "AstroTutorialHostedZone", {
       zoneName: domainName,
     })
+    new cdk.CfnOutput(this, "Site", { value: 'https://' + domainName })
 
-    new cdk.CfnOutput(this, "Site", { value: 'https://' + siteDomain })
-
-    const zone = cdk.aws_route53.HostedZone.fromLookup(this, 'Zone', { domainName: domainName })
-
-    const certificate = new cdk.aws_certificatemanager.Certificate(this, "SiteCertificate", {
-      domainName: domainName,
-      subjectAlternativeNames: ['*.' + domainName],
+    const rootZone = cdk.aws_route53.HostedZone.fromLookup(this, 'RootZone', {
+      domainName: 'aws.skylinedigital.co.nz',
     })
 
-    certificate.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY)
-    new cdk.CfnOutput(this, 'Certificate', { value: certificate.certificateArn })
+    new cdk.aws_route53.NsRecord(this, 'RootZoneNSRecord', {
+      zone: rootZone,
+      recordName: "astro-tutorial",
+      values: zone.hostedZoneNameServers!,
+      ttl: cdk.Duration.days(2),
+    })
+
+    const certificate = cdk.aws_certificatemanager.Certificate.fromCertificateArn(this, 'RootCertificate', "arn:aws:acm:us-east-1:425225284125:certificate/29f34ea6-4950-43f9-874a-cf53a80c7f7f")
 
     const siteBucket = new cdk.aws_s3.Bucket(this, "AstroTutorialSiteBucket", {
       bucketName: "s3-p-ase2-astro-tutorial-dist",
-      publicReadAccess: false,
-      blockPublicAccess: cdk.aws_s3.BlockPublicAccess.BLOCK_ALL,
+      objectOwnership: cdk.aws_s3.ObjectOwnership.BUCKET_OWNER_PREFERRED,
+      publicReadAccess: true,
+      blockPublicAccess: cdk.aws_s3.BlockPublicAccess.BLOCK_ACLS_ONLY,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       websiteIndexDocument: 'index.html',
     })
-    new cdk.CfnOutput(this, 'Bucket', { value: siteBucket.bucketName });
+    new cdk.CfnOutput(this, 'SiteBucketName', { value: siteBucket.bucketName });
 
     const distribution = new cdk.aws_cloudfront.Distribution(this, "AstroTutorialDistribution", {
-      certificate: certificate,
-      domainNames: [siteDomain, domainName],
+      certificate,
+      domainNames: [domainName],
       minimumProtocolVersion: cdk.aws_cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
       defaultBehavior: {
-        origin: cdk.aws_cloudfront_origins.S3BucketOrigin.withOriginAccessControl(siteBucket),
+        origin: new cdk.aws_cloudfront_origins.S3StaticWebsiteOrigin(siteBucket),
         compress: true,
         allowedMethods: cdk.aws_cloudfront.AllowedMethods.ALLOW_GET_HEAD,
         viewerProtocolPolicy: cdk.aws_cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS, },
@@ -47,19 +49,13 @@ export class CdkStack extends cdk.Stack {
     })
     new cdk.CfnOutput(this, 'DistributionId', { value: distribution.distributionId })
 
-    new cdk.aws_route53.ARecord(this, 'WWWSiteAliasRecord', {
-      zone,
-      recordName: siteDomain,
-      target: cdk.aws_route53.RecordTarget.fromAlias(new cdk.aws_route53_targets.CloudFrontTarget(distribution)),
-    })
-
-    new cdk.aws_route53.ARecord(this, 'SiteAliasRecord', {
+    new cdk.aws_route53.ARecord(this, 'AstroTutorialSiteAliasRecord', {
       zone,
       recordName: domainName,
       target: cdk.aws_route53.RecordTarget.fromAlias(new cdk.aws_route53_targets.CloudFrontTarget(distribution)),
     })
 
-    new cdk.aws_s3_deployment.BucketDeployment(this, 'DeployAstroTutorialSite', {
+    new cdk.aws_s3_deployment.BucketDeployment(this, 'AstroTutorialDeploySite', {
       sources: [cdk.aws_s3_deployment.Source.asset('../../dist')],
       destinationBucket: siteBucket,
       distribution,
